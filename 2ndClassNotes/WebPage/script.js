9f324b846eacac0dee450c523041326d8b2276ac
Chris Desmarais <desmarais_chris@hotmail.com>
2023-11-17 12:02:25 -0700

    Added classnotes for session 2

---
+++ 2ndClassNotes/WebPage/script.js
@@ -0,0 +1,49 @@
+window.onload = loadData()
+
+async function loadData() {
+    console.log('loading data')
+    try {
+        const fetchRes = await fetch('http://localhost:3000/messages')
+        const jsonRes = await fetchRes.json()
+        displayMessages(jsonRes)
+    } catch (err) {
+        console.log('fetch error', err)
+    }
+}
+
+function displayMessages(messages){
+    var messageDiv = document.getElementById('messages')
+    messageDiv.innerHTML = ''
+    messages.forEach(msg => {
+        var newMessageDiv = document.createElement('div')
+        newMessageDiv.textContent = msg.userName + ' - '+ msg.message
+        messageDiv.append(newMessageDiv)
+    });
+}
+
+async function sendMessage(){
+    var newMessageInput = document.getElementById('newMessage')
+    const text = newMessageInput.value
+
+    var userName = document.getElementById('userName').value
+    await sendMessageToServer(text, userName)
+    newMessageInput.value = ''
+    loadData()
+}
+async function sendMessageToServer(message, userName){
+    try {
+        // {messasge: message}
+        const messageContent = {message}
+        const response = await fetch('http://localhost:3000/messages?userName='+userName, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(messageContent)
+        })
+        if (!response.ok)
+            alert('Failed to create message: '+ response.status +': '+response.statusText)
+    } catch(error) {
+        console.log("unexpected error", error)
+    }
+}
